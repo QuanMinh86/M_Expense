13f566f0d363eb538a58a11bc2a559b5fa508415
crayntd <84957601+crayntd@users.noreply.github.com>
2022-12-05 16:00:00 +0700

    M_Expense

---
+++ CW_2_Sample_123/app/src/main/java/vn/edu/greenwich/cw_2_sample_123/MainActivity.java
@@ -0,0 +1,131 @@
+package vn.edu.greenwich.cw_2_sample_123;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.squareup.picasso.Picasso;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    protected static final String _FILE_NAME = "cw_2_image_list.txt";
+
+    protected ImageView imageView;
+    protected EditText etImageLink;
+    protected Button btnPrevious, btnNext, btnAdd;
+
+    protected ArrayList<String> _imageList;
+    protected int _currentIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        etImageLink = findViewById(R.id.etImageLink);
+        btnPrevious = findViewById(R.id.btnPrevious);
+        btnNext = findViewById(R.id.btnNext);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(v -> addImage());
+        btnNext.setOnClickListener(v -> nextImage());
+        btnPrevious.setOnClickListener(v -> previousImage());
+
+        _imageList = getImageList();
+        _currentIndex = 0;
+
+        loadImage();
+    }
+
+    protected ArrayList<String> getImageList() {
+        ArrayList<String> imageList = new ArrayList<>();
+
+        imageList.add("https://image.thanhnien.vn/1200x630/Uploaded/2022/vjryqdxwp/2022_08_29/bia-1318.png");
+        imageList.add("https://icolor.vn/wp-content/uploads/2021/12/bia-sai-gon.jpg");
+        imageList.add("https://product.hstatic.net/200000450515/product/upload_c49fcf120cb142268521ea8a12306016_master.jpg");
+
+        getImageListFromFile(imageList);
+
+        Toast.makeText(this, "Get list successfully.", Toast.LENGTH_SHORT).show();
+
+        return imageList;
+    }
+
+    protected void loadImage() {
+        Picasso.with(this).load(_imageList.get(_currentIndex)).into(imageView);
+    }
+
+    protected void addImage() {
+        String imageURL = etImageLink.getText().toString();
+
+        _imageList.add(imageURL);
+        writeURLToFile(imageURL);
+
+        etImageLink.setText("");
+
+        Toast.makeText(this, "Added successfully.", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void nextImage() {
+        ++_currentIndex;
+        if(_currentIndex == _imageList.size())
+        {
+            _currentIndex = 0;
+        }
+        loadImage();
+    }
+
+    protected void previousImage() {
+        --_currentIndex;
+        if(_currentIndex < 0)
+        {
+            _currentIndex = _imageList.size()-1;
+        }
+        loadImage();
+    }
+
+    protected void writeURLToFile(String url) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(_FILE_NAME, MODE_APPEND));
+            outputStreamWriter.write(url);
+            outputStreamWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "File not found.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getImageListFromFile(ArrayList<String> imageList) {
+        try {
+            InputStream inputStream = openFileInput(_FILE_NAME);
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String url = "";
+                while ((url = bufferedReader.readLine()) != null) {
+                    imageList.add(url);
+                }
+
+                inputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "File not found.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
